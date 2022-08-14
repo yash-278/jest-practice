328f9bf568120d672f99051f37ecc286922d32ad
yash-278 <yashkadam278@gmail.com>
2022-08-14 15:56:38 +0530

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,18 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import Banner from "./components/Banner/Banner";
+import { Routes, Route } from "react-router-dom";
+import TodoPage from "./pages/TodoPage/TodoPage";
+import FollowersPage from "./pages/FollowersPage/FollowersPage";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Banner />
+      <Routes>
+        <Route strict path="/" element={<TodoPage />} />
+        <Route strict exact path="/followers" element={<FollowersPage />} />
+      </Routes>
     </div>
   );
 }
